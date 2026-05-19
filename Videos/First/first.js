6a9df505c6a11a1b761c27f704a7497ffc7fec03
jaykum1210 <jaykumawat1210@gmail.com>
2026-05-19 15:23:23 +0530

    First Video

---
+++ Videos/First/first.js
@@ -0,0 +1,27 @@
+console.log("Hello, My Name is Jay Kumawat");
+name = "Jay Kumawat";
+age = 20;
+console.log("Name : " + name + " Age : " + age);
+
+isFollow = false;
+console.log(isFollow);
+
+let fullname = "Jay Kumawat";
+console.log(fullname);
+
+const pi = 3.14;
+// pi = 3;
+console.log(pi);
+
+console.log(typeof(fullname));
+
+
+const student = {
+    fullname: "Jay Kumawat",
+    age : 20,
+    cgpa : 8.5
+};
+
+console.log(student);
+console.log(student.fullname);
+console.log(student["fullname"]);
