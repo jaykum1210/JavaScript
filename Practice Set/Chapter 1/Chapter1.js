e3e1534bc54de1df51429a0d677e71a525eef9ae
Jay Kumawat <jaykumawat1210@gmail.com>
2026-05-25 13:10:31 +0530

    Practice set 1

---
+++ Practice Set/Chapter 1/Chapter1.js
@@ -0,0 +1,36 @@
+// Question 1 Make String and add 1
+
+name = "Jay";
+console.log(name+1);
+
+// Question 2 type of new string 
+
+console.log(typeof name);
+
+//Question 3 Const and try to update
+
+const pi = 3.14;
+// pi = 4;   Not Possible
+console.log(pi);
+
+//Question 4 Try to add new value in const
+
+const obj = {
+    name : "Jay",
+    age : 20,
+};
+
+obj['year']=2;
+console.log(obj);
+
+//Question 5 create word-meaning dictionary
+
+const word = {
+    comfortable : "Comfortable furniture and clothes provide a pleasant feeling and do not give you any physical problems",
+    desert : "an area, often covered with sand or rocks, where there is very little rain and not many plants",
+    rocks : "used to show that you like or approve of someone or something a lot",
+    approve : "to have a positive opinion of someone or something",
+    opinion : "a thought or belief about something or someone"
+}
+
+console.log(word);
